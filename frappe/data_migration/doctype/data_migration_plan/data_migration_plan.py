a6e43a973a2b1e7fed9aa2c1f74ee261cbdb2e65
Zlash65 <zarrar65@gmail.com>
2017-08-17 15:57:33 +0530

    refined code

--- frappe/data_migration/doctype/data_migration_plan/data_migration_plan.py
+++ frappe/data_migration/doctype/data_migration_plan/data_migration_plan.py
@@ -19,49 +19,32 @@ class DataMigrationPlan(Document):
 
 			for d in app.mappings:
 				# iterating through each mappings
-				mapping = frappe.get_doc('Data Migration Mapping', d.mapping)
-				data = source_connector.get_objects(mapping.source_objectname, mapping.condition, "*")
-				make_custom_fields(mapping.target_doctype) # Creating a custom field for primary key
+				self.mapping = frappe.get_doc('Data Migration Mapping', d.mapping)
+				data = source_connector.get_objects(self.mapping.source_objectname, self.mapping.condition, "*")
+				self.make_custom_fields(self.mapping.target_doctype) # Creating a custom field for primary key
 
 				for i, source in enumerate(data):
-					# Checking if the current data object has previously been set or not
-					flag = frappe.db.get_value(mapping.target_doctype, {'migration_key': source.get('id')})
-
-					if flag:
-						# If it is, then fetch that docktype
-						target = frappe.get_doc(mapping.target_doctype, flag)
-					else:
-						# If not, then check if a data by that name already exist or not
-						primary_name = mapping.mapping_details[0].target_fieldname
-						primary_value = source.get(mapping.mapping_details[0].source_fieldname)
-						flag_2 = frappe.db.get_value(mapping.target_doctype, {primary_name: primary_value})
-
-						if flag_2:
-							# If same name is found, fetch that doctype
-							target = frappe.get_doc(mapping.target_doctype, flag_2)
-						else :
-							#  Else create a new doctype for current data object
-							target = frappe.new_doc(mapping.target_doctype)
+					# Fetchnig the appropriate doctype
+					target = self.fetch_doctype(source)
 
 					target.set('migration_key', source.get('id')) # Setting migration key
 
 					# Iterating through each field to map
-					for field in mapping.mapping_details:
+					for field in self.mapping.mapping_details:
 						source_field = field.source_fieldname
 
 						# If source field contains a dot linkage, then its a foreign key relation
 						if '.' in  source_field:
 							arr = source_field.split('.')
-							join_data = source_connector.get_join_objects(mapping.source_objectname, field, source.get('id'))
-
+							join_data = source_connector.get_join_objects(self.mapping.source_objectname, field, source.get('id'))
 							target.set(field.target_fieldname, join_data[0][arr[1]])
 						else:
 							# Else its a simple column to column mapping
-							target.set(field.target_fieldname, source.get(source_field))
+							target.set(field.target_fieldname, frappe.as_unicode(source.get(source_field)))
 
 					# post process
-					if mapping.post_process:
-						exec mapping.post_process in locals()
+					if self.mapping.post_process:
+						exec self.mapping.post_process in locals()
 
 					try:
 						target.save()
@@ -75,7 +58,39 @@ class DataMigrationPlan(Document):
 				frappe.publish_progress(100,
 					title = _('Migrating {0}').format(target.doctype), doctype=self.doctype, docname=self.name)
 
+	def make_custom_fields(self, dt):
+		""" Adding custom field for primary key """
+		field = frappe.db.get_value("Custom Field", {"dt": dt, "fieldname": 'migration_key'})
+		if not field:
+			create_custom_field(dt, {
+				'label': 'Migration Key',
+				'fieldname': 'migration_key',
+				'fieldtype': 'Data',
+				'hidden': 1,
+				'read_only': 1,
+				'unique': 1,
+			})
 
+	def fetch_doctype(self, source):
+		""" Returns correct doctype type - new or existing """
+		flag = frappe.db.get_value(self.mapping.target_doctype, {'migration_key': source.get('id')})
+
+		if flag:
+			# If it is, then fetch that docktype
+			return frappe.get_doc(self.mapping.target_doctype, flag)
+		else:
+			# If not, then check if a data by that name already exist or not
+			primary_name = self.mapping.mapping_details[0].target_fieldname
+			primary_value = source.get(self.mapping.mapping_details[0].source_fieldname)
+
+			flag_2 = frappe.db.get_value(self.mapping.target_doctype, {primary_name: primary_value})
+
+			if flag_2:
+				# If same name is found, fetch that doctype
+				return frappe.get_doc(self.mapping.target_doctype, flag_2)
+			else :
+				#  Else create a new doctype for current data object
+				return frappe.new_doc(self.mapping.target_doctype)
 
 @frappe.whitelist()
 def migrate(plan):
@@ -86,16 +101,3 @@ def migrate(plan):
 	plan.migrate()
 
 	frappe.clear_messages()
-
-@frappe.whitelist()
-def make_custom_fields(dt):
-	field = frappe.db.get_value("Custom Field", {"dt": dt, "fieldname": 'migration_key'})
-	if not field:
-		create_custom_field(dt, {
-			'label': 'Migration Key',
-			'fieldname': 'migration_key',
-			'fieldtype': 'Data',
-			'hidden': 1,
-			'read_only': 1,
-			'unique': 1,
-		})
